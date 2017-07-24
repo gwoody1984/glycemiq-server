dde927ea06c15377a2d16e0a5162d45283051ab2
Greg Woodbury <woodbury.greg@gmail.com>
2017-07-24 18:13:28 -0400

    initial config values

--- glycemiq_server/config.py
+++ glycemiq_server/config.py
@@ -1,10 +1,14 @@
 import os
+import configparser
 
+config = configparser.ConfigParser()
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class DevelopmentConfig():
     DEBUG = True
+
+    with open('config.json')
 
 
 class ProductionConfig():
