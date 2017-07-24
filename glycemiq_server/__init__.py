60c2b43393c897308f9b01c10c4e3b61639b68bb
Greg Woodbury <woodbury.greg@gmail.com>
2017-07-24 18:21:54 -0400

    config using configparser

--- glycemiq_server/__init__.py
+++ glycemiq_server/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
 
-from .config import config
+from .config import config_section
 
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(config['FLASK'])
+    app.config.from_object(config_section('FLASK'))
 
     from .fitbit import fitbit as fitbit_blueprint
     app.register_blueprint(fitbit_blueprint, url_prefix='/fitbit')
--- glycemiq_server/config.py
+++ glycemiq_server/config.py
@@ -7,3 +7,6 @@ config = configparser.ConfigParser()
 if os.path.exists('config.json'):
     with open('config.json', 'r') as file:
         config = json.load(file)
+
+def config_section(section):
+    return config[section]
