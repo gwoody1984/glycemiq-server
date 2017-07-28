a142194a93385c85fda5fc57922b8b843b15a846
Greg Woodbury <gwoodbury@highvistastrategies.com>
2017-07-28 10:28:26 -0400

    added notification logging

--- glycemiq_server/fitbit/NotificationActor.py
+++ glycemiq_server/fitbit/NotificationActor.py
@@ -1,18 +1,52 @@
-import ast
+from datetime import datetime
 
 from thespian.actors import *
 
-from ..log_manager import logManager
-
+from glycemiq_server.models import db, Notification
+from glycemiq_server.log_manager import logManager
+from glycemiq_server.fitbit.ActivityActor import ActivityActor
+from glycemiq_server.fitbit.BodyActor import BodyActor
+from glycemiq_server.fitbit.FoodActor import FoodActor
+from glycemiq_server.fitbit.SleepActor import SleepActor
 
 logger = logManager.get_logger(__name__)
 
 
 class NotificationActor(Actor):
+    def __init__(self):
+        super().__init__()
+        self.children = {
+            'foods': FoodActor,
+            'activities': ActivityActor,
+            'sleep': SleepActor,
+            'body': BodyActor
+        }
+
     def receiveMessage(self, msg, sender):
-        if isinstance(msg, ActorExitRequest):
+        if not isinstance(msg, list):
             return
 
         logger.debug(str(msg))
-        self.send(self.myAddress, ActorExitRequest())
+        msg['date'] = datetime.strptime(msg['date'], "%Y-%m-%d").date()
+        msg['subscriptionId'] = int(msg['subscriptionId'])
 
+        for item in msg:
+            self._route_message(item)
+
+    def _route_message(self, msg):
+        child_type = self.children[msg['collectionType']]
+        if child_type is not None:
+            child_actor = self.createActor(child_type)
+            self.send(child_actor, msg)
+
+    def _save_notification(self, msg):
+        notification = Notification()
+        notification.receive_date = datetime.utcnow()
+        notification.collection_Type = msg['collectionType']
+        notification.user_id = msg['ownerId']
+        notification.date = msg['date']
+        notification.owner_type = msg['ownerType']
+        notification.subscription_id = msg['subscriptionId']
+
+        db.session.add(notification)
+        db.session.commit()
--- glycemiq_server/models.py
+++ glycemiq_server/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from glycemiq_server import db
 
 
@@ -10,3 +11,16 @@ class UserToken(db.Model):
 
     def __repr__(self):
         return "<UserToken '{}': '{}'>".format(self.id, self.user_id)
+
+
+class Notification(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    receive_date = db.Column(db.DateTime, default=datetime.utcnow)
+    collection_Type = db.Column(db.String(25), nullable=False)
+    date =db.Column(db.Date, nullable=False)
+    user_id = db.Column(db.String(6), nullable=False)
+    owner_type = db.Column(db.String(25), nullable=False)
+    subscription_id = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return "<Notification '{:%Y-%m-%d}' '{}': '{}'>".format(self.date, self.collection_Type, self.user_id)
