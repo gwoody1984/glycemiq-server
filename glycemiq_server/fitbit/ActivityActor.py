b2553c735d3f551232c53bd26b876a958ea0b104
Greg Woodbury <woodbury.greg@gmail.com>
2017-07-29 10:02:22 -0400

    getting activity data from fitbit to our db

--- glycemiq_server/fitbit/ActivityActor.py
+++ glycemiq_server/fitbit/ActivityActor.py
@@ -1,7 +1,10 @@
+from datetime import datetime
+
 from thespian.actors import ActorExitRequest
 
 from glycemiq_server.fitbit.FitbitDataActor import FitbitDataActor
 from glycemiq_server.log_manager import logManager
+from glycemiq_server.models import db, Activity
 
 logger = logManager.get_logger(__name__)
 
@@ -18,8 +21,30 @@ class ActivityActor(FitbitDataActor):
         self.server.set_fitbit_client(user_id)
         data = self.server.get_activities(user_id, date)
 
-        self._save_activity(data)
+        self._save_activity(user_id, date, data)
         self.send(self, ActorExitRequest())
 
-    def _save_activity(self, data):
+    def _save_activity(self, user_id, date, data):
         logger.debug(str(data))
+
+        if not isinstance(data, dict):
+            logger.error("Expected dict, got {}".format(str(type(data))))
+            return
+
+        summarySection = data['summary']
+
+        activity = Activity()
+        activity.receive_date = datetime.utcnow()
+        activity.user_id = user_id
+        activity.date = date
+        activity.steps = summarySection['steps']
+        activity.restingHeartRate = summarySection['restingHeartRate']
+        activity.sedentaryMinutes = summarySection['sedentaryMinutes']
+        activity.lightlyActiveMinutes = summarySection['lightlyActiveMinutes']
+        activity.fairlyActiveMinutes = summarySection['fairlyActiveMinutes']
+        activity.veryActiveMinutes = summarySection['veryActiveMinutes']
+        activity.caloriesOut = summarySection['caloriesOut']
+
+        db.session.add(activity)
+        db.session.commit()
+
--- glycemiq_server/models.py
+++ glycemiq_server/models.py
@@ -24,3 +24,20 @@ class Notification(db.Model):
 
     def __repr__(self):
         return "<Notification '{:%Y-%m-%d}' '{}': '{}'>".format(self.date, self.collection_Type, self.user_id)
+
+
+class Activity(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    receive_date = db.Column(db.DateTime, default=datetime.utcnow)
+    date = db.Column(db.Date, nullable=False)
+    user_id = db.Column(db.String(6), nullable=False)
+    steps = db.Column(db.Integer, nullable=False)
+    restingHeartRate = db.Column(db.Integer, nullable=False)
+    sedentaryMinutes = db.Column(db.Integer, nullable=False)
+    lightlyActiveMinutes = db.Column(db.Integer, nullable=False)
+    fairlyActiveMinutes = db.Column(db.Integer, nullable=False)
+    veryActiveMinutes = db.Column(db.Integer, nullable=False)
+    caloriesOut = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return "<Activity '{:%Y-%m-%d}': '{}'>".format(self.date, self.user_id)
